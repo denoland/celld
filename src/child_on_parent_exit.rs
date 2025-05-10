1d1dc24ddacca3394cf822212107236cb2f5d8c4
Ryan Dahl <ry@tinyclouds.org>
2025-05-10 04:49:30 -0700

    fix warnings on linux

--- src/child_on_parent_exit.rs
+++ src/child_on_parent_exit.rs
@@ -1,10 +1,8 @@
-use nix::fcntl::{fcntl, FcntlArg, FdFlag};
 use nix::sys::signal::{kill, Signal};
 use nix::sys::wait::waitpid;
-use nix::unistd::{fork, pipe, read, ForkResult, Pid};
+use nix::unistd::Pid;
 use std::io;
 use std::os::fd::OwnedFd;
-use std::os::unix::io::AsRawFd;
 use std::process::Command;
 
 #[cfg(target_os = "linux")]
@@ -31,11 +29,13 @@ impl ChildOnParentExit {
     #[cfg(target_os = "linux")]
     {
       use nix::sys::prctl::set_pdeathsig;
-      cmd.before_exec(|| {
-        set_pdeathsig(Some(Signal::SIGTERM))
-          .map_err(|e| io::Error::new(io::ErrorKind::Other, e))?;
-        Ok(())
-      });
+      unsafe {
+        cmd.pre_exec(|| {
+          set_pdeathsig(Some(Signal::SIGTERM))
+            .map_err(|e| io::Error::new(io::ErrorKind::Other, e))?;
+          Ok(())
+        });
+      }
 
       let child = cmd.spawn()?;
       Ok(ChildOnParentExit {
@@ -46,6 +46,10 @@ impl ChildOnParentExit {
 
     #[cfg(all(unix, not(target_os = "linux")))]
     {
+      use nix::fcntl::{fcntl, FcntlArg, FdFlag};
+      use nix::unistd::{fork, pipe, read, ForkResult};
+      use std::os::unix::io::AsRawFd;
+
       // create a pipe; parent holds w, watcher holds r
       let (r, w) =
         pipe().map_err(|e| io::Error::new(io::ErrorKind::Other, e))?;
