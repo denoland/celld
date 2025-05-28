2de3df9598382672af0f8cad3649cc7e21bbdb8f
Yusuke Tanaka <wing0920@gmail.com>
2025-05-28 11:42:02 +0900

    add test

--- jsr-cells/workflow_test.ts
+++ jsr-cells/workflow_test.ts
@@ -18,5 +18,29 @@ Deno.test("Workflow type", () => {
   type WorkflowNames = Parameters<typeof workflow.define>[0]["name"];
   assertType<IsExact<WorkflowNames, "user.signup" | "user.login">>(true);
 
-  // Can we test that `event.data` is strictly typed depending on the workflow name?
+  // Test that the user.signup's event.data parameter is correctly typed
+  type UserSignupHandler = Parameters<
+    typeof workflow.define<"user.signup">
+  >[0]["handler"];
+  type UserSignupHandlerEventArg = Parameters<UserSignupHandler>[0]["event"];
+  assertType<IsExact<UserSignupHandlerEventArg["name"], "user.signup">>(true);
+  assertType<
+    IsExact<
+      UserSignupHandlerEventArg["data"],
+      { userId: string; email: string }
+    >
+  >(true);
+
+  // Test that the user.login's event.data parameter is correctly typed
+  type UserLoginHandler = Parameters<
+    typeof workflow.define<"user.login">
+  >[0]["handler"];
+  type UserLoginHandlerEventArg = Parameters<UserLoginHandler>[0]["event"];
+  assertType<IsExact<UserLoginHandlerEventArg["name"], "user.login">>(true);
+  assertType<
+    IsExact<
+      UserLoginHandlerEventArg["data"],
+      { userId: string }
+    >
+  >(true);
 });
