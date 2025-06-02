3361e5845fd5897cabf924ca801f997ec5f40c93
Ryan Dahl <ry@tinyclouds.org>
2025-06-02 13:50:01 -0700

    fix typescript error (#73)

--- jsr-cells/workflow.ts
+++ jsr-cells/workflow.ts
@@ -217,7 +217,7 @@ export class Workflow<WorkflowInputs extends Record<string, JSONValue>> {
       completedAt: runResult.completed_at
         ? new Date(runResult.completed_at as string)
         : null,
-      steps: stepsResult.map((row: any) => {
+      steps: stepsResult.map((row) => {
         return {
           stepIndex: row.step_index as number,
           name: row.name as string,
