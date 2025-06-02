9340e386b79482a6fe39cd22d7703dc0c97eac59
Yusuke Tanaka <wing0920@gmail.com>
2025-06-02 14:22:53 +0900

    lint

--- jsr-cells/workflow.ts
+++ jsr-cells/workflow.ts
@@ -24,7 +24,7 @@ export class Workflow<WorkflowInputs extends Record<string, JSONValue>> {
     >["handler"];
   } = {};
 
-  static runningWorkflows() {
+  static runningWorkflows(): number {
     return this.#runningWorkflows;
   }
 
