1107d82de617af0d0073372882c8e29ec43a5133
Ryan Dahl <ry@tinyclouds.org>
2025-06-10 15:24:26 -0700

    undo changes to channel_cell.ts since it uses published version

--- demos/cellverse/cells/channel_cell.ts
+++ demos/cellverse/cells/channel_cell.ts
@@ -1,4 +1,4 @@
-import { cell } from "@ry/cells";
+import { cell, define, dispatch } from "@ry/cells";
 import { z } from "npm:zod";
 import { extractBearerToken, verifyJWT } from "./utils.ts";
 
@@ -89,7 +89,7 @@ if (cell.id.startsWith("channel-")) {
     }),
   ]);
 
-  const reasoningWorkflow = cell.workflow.define<{
+  const reasoningWorkflow = define<{
     messages: {
       role: "user" | "assistant";
       content: string;
@@ -659,7 +659,7 @@ Respond naturally while occasionally storing or retrieving memories.`;
               }));
 
               // Start reasoning
-              cell.workflow.dispatch(reasoningWorkflow, {
+              dispatch(reasoningWorkflow, {
                 messages,
                 allowedSteps: 5,
               });
