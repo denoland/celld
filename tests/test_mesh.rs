ae63610b80fe11d62a9d2d025f20356aca1910dc
Yusuke Tanaka <wing0920@gmail.com>
2025-06-12 20:30:42 +0900

    try without flaky_test macro

--- tests/test_mesh.rs
+++ tests/test_mesh.rs
@@ -414,7 +414,7 @@ async fn test_node_failure_takeover() {
 }
 
 /// Tests concurrent takeover attempts to verify only one node succeeds via locking
-#[flaky_test::flaky_test(tokio)]
+#[test_log::test(tokio::test)]
 async fn test_concurrent_takeover_locking() {
   // Setup three celld nodes in the mesh with auto-allocated ports
   let mut test_env = TestEnv::new(3);
